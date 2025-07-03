ca326f0e9522ab73a53c96482319596e446f4e84
Marwan878 <marwanmohammad432@gmail.com>
2025-07-03 12:02:37 +0300

    Fixes

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,12 +1,8 @@
 import { Suspense } from "react";
 import CabinList from "../_components/CabinList";
-import Spinner from "../_components/Spinner";
-import Counter from "../_components/Counter";
 import Filter from "../_components/Filter";
 import ReservationReminder from "../_components/ReservationReminder";
-
-export const revalidate = 3600;
-// export const revalidate = 15;
+import Spinner from "../_components/Spinner";
 
 export const metadata = {
   title: "Cabins",
